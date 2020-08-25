09a2987dbcd00513c971e691fd032d7cd5c5ce43
hanzo-genji <chenjinjin233@outlook.com>
2020-08-25 10:01:22 +0800

    搞定 三个 icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 import React from "react";
 import { Link } from "react-router-dom";
-import x from 'icons/money.svg';
-
-console.log(x)
+import { isCompositeComponent } from "react-dom/test-utils";
+require('icons/money.svg');
+require('icons/tag.svg');
+require('icons/chart.svg');
 
 const NavWrapper = styled.nav`
 line-height: 24px;
@@ -13,7 +14,15 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
   > li{
     width: 33.3333%;
     text-align: center;
-    padding: 16px;
+    display: flex;
+    flex-direction: column;
+    padding: 4px 0;
+    justify-content: center;
+    align-items: center;
+    .icon{
+      width: 24px;
+      height: 24px;
+    }
   }
 }
 `;
@@ -23,12 +32,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#tag" />
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money" />
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart" />
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
