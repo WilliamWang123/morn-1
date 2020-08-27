82f3a40c6e86189ed14519407303cf387f9d0fec
hanzo-genji <chenjinjin233@outlook.com>
2020-08-27 23:04:11 +0800

    数字面板按钮功能基本完成

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -59,7 +59,15 @@ box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
 `
 
 const NumberPadSection: React.FC = () => {
-  const [output, setOutput] = useState('0')
+  const [output, _setOutput] = useState('0')
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0, 16);
+    } else if (output.length === 0) {
+      output = '0';
+    }
+    _setOutput(output);
+  }
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent
     if (text === null) { return; }
@@ -74,18 +82,26 @@ const NumberPadSection: React.FC = () => {
       case '7':
       case '8':
       case '9':
-      case '.':
+
         if (output === '0') {
           setOutput(text);
         } else {
           setOutput(output + text)
         }
         break;
+      case '.':
+        if (output.indexOf('.') >= 0) { return; }
+        setOutput(output + '.');
+        break;
       case '删除':
-        console.log('删除');
+        if (output.length === 1) {
+          setOutput('');
+        } else {
+          setOutput(output.slice(0, -1));
+        }
         break;
       case '清空':
-        console.log('清空')
+        setOutput('')
         break;
       case 'OK':
         console.log('确认');
