afe4bc305b4692ea174609b288fca18f00292cd4
hanzo-genji <chenjinjin233@outlook.com>
2020-08-28 12:50:18 +0800

    tags 样式做完

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -3,9 +3,7 @@ import { useState } from 'react';
 
 const useTags = () => {
   const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
-  return {
-    tags, setTags
-  }
+  return { tags, setTags }
 }
 
 export { useTags }
